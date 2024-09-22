794a103249a1052a79279e914977618bd23d01a1
Dylan Lai <dylan.lai@stonybrook.edu>
2024-09-21 20:21:38 -0400

    Submit for Colorforce

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { React } from 'react'
+
+const App = () => {
+  return (
+    <iframe url="https://www.ratemyprofessors.com/school/958"></iframe>
+  );
+}
+
+export default App;
